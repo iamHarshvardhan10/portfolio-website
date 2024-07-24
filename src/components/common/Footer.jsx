e45347ac5cde18d3843aa342aa18eb42f87d8a52
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-24 20:40:18 +0530

    footer page design

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,9 +1,11 @@
-
-
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex">
+      <h3>All Rights are Reserved</h3>
+      <h3>Made by @harsh.devcodes</h3>
+      <span>2024 Portfolio</span>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
