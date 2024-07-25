db524844efc60893a7e20f695aab768244f7a113
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-25 23:57:51 +0530

    connecting mongodb to backend

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+
 
 const app = express()
 
@@ -13,6 +19,14 @@ app.get('/', (req, res) => {
 
 app.listen(8080, () => {
     console.log('Server is running on port 8080')
+})
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('MongoDb Connected')
+}).catch((error) => {
+    console.log('MongoDB Connection failed')
+    console.log(error)
+    process.exit(1)
 })
 
 
