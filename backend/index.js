86a718b1679f7c3e4a0c72341595633b4c3dea33
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-25 20:19:51 +0530

    initialize the backend setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    })
+})
+
+
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
+
+
