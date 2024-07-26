fb04a751b99a3b9f897e186b5aa0ba8649e12698
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-26 17:20:21 +0530

    design home page UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import Typewriter from "typewriter-effect";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="px-[48px]">
+      <h1 className="text-[48px] font-semibold">Hey, I'm Harshvardhan</h1>
+      <h2 className="flex gap-2 text-[48px] font-semibold">
+        I Design{" "}
+        <Typewriter
+          options={{
+            strings: ["Website", "apps", "Software"],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </h2>
+      <p className="mt-[24px] text-[16px] font-medium w-[70%]">
+        Harshvardhan is an experienced Developer based in the India known for
+        creating website and apps.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
