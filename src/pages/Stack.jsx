b2666a846359d5f33490cdb5778deba27dafab09
Harshvardhan <shreeharshpb11@gmail.com>
2024-07-26 16:33:38 +0530

    desing stack page UI

--- src/pages/Stack.jsx
+++ src/pages/Stack.jsx
@@ -1,9 +1,28 @@
-
-
 const Stack = () => {
   return (
-    <div>Stack</div>
-  )
-}
+    <div className="px-[36px]">
+      <h1 className="text-[48px] font-semibold">Stack</h1>
+      <p className="text-[16px] mt-[24px] font-medium">
+        Tools I use & love that you should check out.
+      </p>
+      <div className="mt-[52px] flex items-center">
+        <div className="flex">
+          <img src="" alt="" className="w-[40px] h-[40px] bg-black" />
+          <div>
+            <h2>Tool Name</h2>
+            <p>Tool Description</p>
+          </div>
+        </div>
+        <div className="flex">
+          <img src="" alt="" className="w-[40px] h-[40px] bg-black" />
+          <div>
+            <h2>Tool Name</h2>
+            <p>Tool Description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Stack
+export default Stack;
