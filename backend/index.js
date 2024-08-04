2604f7499b757fe44e1ce7b54ff80aa0a457030d
Harshvardhan <shreeharshpb11@gmail.com>
2024-08-04 23:01:25 +0530

    fix cors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://harshdevcodes-portfolio-okys3vbdc.vercel.app/",
         credentials: true
     }))
 // file upload
